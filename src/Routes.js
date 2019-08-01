acbfc3971390e14b2755d41ee1072008c7d78fb6
zlc <1902643396@qq.com>
2019-08-01 18:41:37 +0800

    新内容上传到second分支

--- src/Routes.js
+++ src/Routes.js
@@ -1,27 +1,22 @@
 import React from "react";
 import { Router, Route} from "react-router-dom";
-import { createBrowserHistory } from "history";
-import { nav as NavBar } from "./navBar";
+import { history } from "./utils";
+
 import GoodsList from "./goodsList.js";
 import ShoppingCart from "./shoppingCart.js";
 import Message from "./message.js";
 import Mine from "./mine.js";
 import Detail from "./detail.js";
 
-const history = createBrowserHistory();
+
 const Routes = () => (
-    <Router history={history}>
-      <div>
-        <NavBar />
-        <div>
-          <Route exact path="/" component={GoodsList} />
-          <Route path="/shoppingCart" component = {ShoppingCart}/>
-          <Route path="/message" component = {Message}/>
-          <Route path="/mine" component = {Mine}/>
-          <Route path="/detail" component = {Detail}/>
-        </div>
-      </div>     
-    </Router>
+  <Router history={history}>
+    <Route exact path="/" component={GoodsList} />
+    <Route path="/shoppingCart" component = {ShoppingCart}/>
+    <Route path="/message" component = {Message}/>
+    <Route path="/mine" component = {Mine}/>
+    <Route path="/detail" component = {Detail}/>              
+  </Router>
 )
 
 export default Routes
